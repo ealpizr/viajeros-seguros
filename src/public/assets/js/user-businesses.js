dc6d5c9812267d1ca98ad98301fc24871a291288
Enrique Alpízar <ealpizr@gmail.com>
2023-10-24 09:34:56 -0700

    Add a basic user businesses page design

---
+++ src/public/assets/js/user-businesses.js
@@ -0,0 +1,114 @@
+const TEST_BUSINESSES = [
+  {
+    _id: "1",
+    name: "Test Business 1",
+    description: "Test Business 1 Description",
+    address: "Test Business 1 Address",
+    images: [
+      "https://via.placeholder.com/500x300",
+      "https://via.placeholder.com/500x300",
+      "https://via.placeholder.com/500x300",
+    ],
+    reviews: [
+      {
+        name: "Test User 1",
+        photo: "https://via.placeholder.com/150",
+        rating: 5,
+        comment: "Test User 1 Comment",
+      },
+      {
+        name: "Test User 2",
+        photo: "https://via.placeholder.com/150",
+        rating: 4,
+        comment: "Test User 2 Comment",
+      },
+    ],
+    rating: 4.5,
+    numReviews: 2,
+  },
+  {
+    _id: "2",
+    name: "Test Business 2",
+    description: "Test Business 2 Description",
+    address: "Test Business 2 Address",
+    images: [
+      "https://via.placeholder.com/500x300",
+      "https://via.placeholder.com/500x300",
+      "https://via.placeholder.com/500x300",
+    ],
+    reviews: [
+      {
+        name: "Test User 1",
+        photo: "https://via.placeholder.com/150",
+        rating: 2,
+        comment: "Test User 1 Comment",
+      },
+      {
+        name: "Test User 2",
+        photo: "https://via.placeholder.com/150",
+        rating: 2,
+        comment: "Test User 2 Comment",
+      },
+      {
+        name: "Test User 2",
+        photo: "https://via.placeholder.com/150",
+        rating: 2,
+        comment: "Test User 2 Comment",
+      },
+    ],
+    rating: 2,
+    numReviews: 3,
+  },
+];
+
+function getRatingStars(rating) {
+  const halfStar = rating % 1 === 0.5;
+  return "★".repeat(rating + halfStar) + "☆".repeat(5 - rating);
+}
+
+const businessesContainer = document.getElementById("businesses-container");
+for (let i = 0; i < TEST_BUSINESSES.length; ++i) {
+  const b = TEST_BUSINESSES[i];
+
+  const el = `<div class="business-card">
+    <div class="business-card-section">
+      <h2>${b.name}</h2>
+      <p>${b.description}</p>
+      <p>${b.address}</p>
+    </div>
+    <div class="business-card-section">
+      <h3>Imágenes</h3>
+      <div class="business-images-container">
+        ${b.images
+          .map(
+            (img) => `<div class="business-image"><img src="${img}" /></div>`
+          )
+          .join("")}
+      </div>
+    </div>
+    <div class="business-card-section">
+      <h3>Feedback de reseñas</h3>
+      <div class="rating-container">
+        <p>${b.rating}</p>
+        <p>${getRatingStars(b.rating)}</p>
+        <p>${b.numReviews} reseñas</p>
+      </div>
+      <div class="reviews-container">
+        ${b.reviews
+          .map(
+            (r) => `<div class="review">
+          <div class="review-image">
+            <img src="${r.photo}" />
+          </div>
+          <div>
+            <p>${r.name}</p>
+            <p>${r.comment}</p>
+          </div>
+          </div>`
+          )
+          .join("")}
+      </div>
+    </div>`;
+
+  businessesContainer.innerHTML += el;
+}
