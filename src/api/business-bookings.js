bd6bc529863123fd0f8d41859f570ff5922e3b6d
Isa <inassarm@ucenfotec.ac.cr>
2023-12-04 20:18:56 -0600

    Business bookings

---
+++ src/api/business-bookings.js
@@ -0,0 +1,28 @@
+import User from "../schemas/user.js";
+import Reservation from "../schemas/reservation.js";
+const userId = "65655817c7cfd62135ae90a4";
+
+export async function listBusinessReservations(req, res) {
+  try {
+    User.findById(userId)
+      .exec()
+      .then(function (user) {
+        const reservations = [];
+        const reservationIds = user.reservations;
+        reservationIds.forEach(Id => {
+          Reservation.findById(Id).exec().then(function (reservation) {
+            console.log(reservation)
+            reservations.push(reservation)
+          });
+        });
+
+        res.json(reservations);
+      })
+
+
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: "Error al listar las reservas" });
+  }
+}
+
